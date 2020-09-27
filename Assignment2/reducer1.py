67be8270bb226c12115c8fdbddf815e7025d171d
Sreyans Bothra <sreyansrocks@gmail.com>
2020-09-27 19:25:19 +0530

    Before convergence and bash script

--- Assignment2/reducer1.py
+++ Assignment2/reducer1.py
@@ -2,7 +2,8 @@
 import sys
 cur_node=""
 cur_list=[]
-v_file = open("v","w")
+location=sys.argv[1]
+v_file = open(location,"w")
 for inputline in sys.stdin:
     line=inputline.strip()
     from_node,to_node=line.split(" ")
@@ -10,23 +11,27 @@ for inputline in sys.stdin:
         if cur_node!="":
             #print(cur_node,cur_list)
             #cur_list.remove(int(cur_node))
-            print(cur_node,sorted(cur_list),sep="\t")
+            cur_list = sorted(cur_list)
+            cur_list = '['+",".join(cur_list)+']'
+            print(cur_node,cur_list,sep="\t")
             cur_list=[]
         cur_node=from_node
         v_file.write(f"{cur_node}, 1\n")
+    cur_list.append(to_node)
+    """
     try:
-        cur_list.append(int(to_node))
+        #cur_list.append(int(to_node))
     except:
         cur_list.append(int("".join(to_node.split(","))))
+    """
 if cur_node:
     #print(cur_node,cur_list)
     #cur_list.remove(int(cur_node))
-    print(cur_node,sorted(cur_list),sep="\t")
+    #print(cur_node,sorted(cur_list),sep="\t")
+    cur_list = sorted(cur_list)
+    cur_list = '['+",".join(cur_list)+']'
+    print(cur_node,cur_list,sep="\t")
 v_file.close()
-
-        
-    
-
 
 
 
--- Assignment2/reducer2.py
+++ Assignment2/reducer2.py
@@ -4,18 +4,25 @@ initnode=""
 initsum=0
 v1_file = open("v1","w")
 for eachline in sys.stdin:
+    #print(eachline)
     line=eachline.strip()
     node,contrib = line.split(" ")
     if initnode!=node:
         if initnode!="":
-            val=round(0.15+0.85*initsum,4)
-            v1_file.write(f"{initnode}, {val}\n")
+            val=round(0.15+0.85*initsum,5)
+            k=str(val)
+            if len(k[k.index("."):])<6:
+                k=k+"0"*(6-len(k[k.index("."):]))
+            v1_file.write(f"{initnode}, {k}\n")
         initnode = node
         initsum = float(contrib)
     else:
         initsum+= float(contrib)
 if initnode:
-    val=round(0.15+0.85*initsum,4)
-    v1_file.write(f"{initnode}, {val}\n")
+    val=round(0.15+0.85*initsum,5)
+    k=str(val)
+    if len(k[k.index("."):])<6:
+                k=k+"0"*(6-len(k[k.index("."):]))
+    v1_file.write(f"{initnode}, {k}\n")
 v1_file.close()
 
