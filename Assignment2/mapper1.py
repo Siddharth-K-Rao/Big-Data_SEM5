9a830cff747273114de4d14949d138a05a1f5a4e
Sreyans Bothra <sreyansrocks@gmail.com>
2020-09-28 21:40:42 +0530

    Final

--- Assignment2/mapper1.py
+++ Assignment2/mapper1.py
@@ -5,5 +5,9 @@ for inputline in sys.stdin:
     line=inputline.strip()
     if line[0]=="#":
         continue
-    from_node,to_node=line.split(" ")
-    print(from_node,to_node,sep=" ")
+    try:
+        from_node,to_node=line.split("\t")
+        print(from_node,to_node,sep=" ")
+    except:
+        print("here")
+        break
