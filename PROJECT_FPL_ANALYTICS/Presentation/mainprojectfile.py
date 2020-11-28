0688acb5ee62eeb105667a329be9b2fe26a19d8c
Sreyans Bothra <sreyansrocks@gmail.com>
2020-11-28 17:50:24 +0530

    New implementation

--- PROJECT_FPL_ANALYTICS/Presentation/mainprojectfile.py
+++ PROJECT_FPL_ANALYTICS/Presentation/mainprojectfile.py
@@ -14,12 +14,14 @@ from pyspark import SparkContext,SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.sql import Row,SQLContext
 
+fuldata={}
+
 def eventupdate(i):
     return
 
 def matchupdate(matchdict,date,label):
     print("\n\nMATCHUPDATE\n\n")
-    match_events='/home/sreyans/Desktop/SEM5/Big_Data_SEM5/PROJECT_FPL_ANALYTICS/match_events.json'
+    #match_events='/home/sreyans/Desktop/SEM5/Big_Data_SEM5/PROJECT_FPL_ANALYTICS/match_events.json'
     datatuple=str(date)+" "+str(label)
     try:
         with open(match_events,"r") as json_file:
@@ -28,6 +30,7 @@ def matchupdate(matchdict,date,label):
                 return
     except:
         fulldata={}
+    fulldata={}
     fulldata[datatuple]={}
     fulldata[datatuple]['date']=date
     fulldata[datatuple]['duration']=matchdict['duration']
@@ -79,9 +82,10 @@ def matchupdate(matchdict,date,label):
                     if int(k['yellowCards']):
                         fulldata[datatuple]['yellow_cards'].append(k["playerId"])
 
-    json_file=open(match_events,"w")
+    json_file=open(match_events,"a")
     finalobj=json.dumps(fulldata,indent=2)
     json_file.write(finalobj)
+    json_file.close()
 
 def splitandcheck(lines):
     print("\n\n",lines) 
@@ -95,8 +99,131 @@ def splitandcheck(lines):
 
 def splitrdd(records):
     if (records.isEmpty()):
+        print("\nEMPTY\n")
         return
     records.foreach(splitandcheck)
+
+def printkaro(records):
+    print("\n\n",records)  
+
+'''
+#native python code ->to preprocess all the players before any match starts
+
+with open('playerprofile.csv', 'w', newline='') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerow(['name','birthArea','birthDate','foot','role','height','passportArea','weight','fouls','goals','own_goals','percent_pass_accuracy','percent_shots_on_target','matches_played'])
+
+    with open('/home/sreyans/Desktop/SEM5/Big_Data_SEM5/PROJECT_FPL_ANALYTICS/play.csv', 'r', newline='') as incsv:
+        reader = csv.reader(incsv)
+        writer.writerows(row + [0,0,0,0,0,0] for row in reader if "name" not in row)
+'''
+conf=SparkConf()
+conf.setAppName('BD_FPL_Project')
+
+sc=SparkContext.getOrCreate(conf=conf)
+#print("DONe")
+ssc=StreamingContext(sc,2) #5s for time difference between two batches - design decision
+ssc.checkpoint('checkpoint_FPL')
+
+lines = ssc.socketTextStream("localhost", 6100)
+#lines.pprint()
+lines.foreachRDD(splitrdd)
+
+
+ssc.start()
+ssc.awaitTermination(100)
+ssc.stop()
+
+'''
+sc = SparkContext("local[1]", "NetworkFPL") #2 represents number of threads
+ssc = StreamingContext(sc, 5) #1 represents the batch difference
+lines = ssc.socketTextStream("localhost", 6100)
+lines.reduce(add).pprint()
+ssc.start()
+ssc.awaitTermination()
+'''
+
+"""
+
+
+
+from pyspark import SparkContext,SparkConf
+
+from pyspark.sql import Row,SQLContext,SparkSession
+
+def eventupdate(i):
+    return
+
+def matchupdate(matchdict,date,label):
+    print("\n\nMATCHUPDATE\n\n")
+    match_events='/home/sreyans/Desktop/SEM5/Big_Data_SEM5/PROJECT_FPL_ANALYTICS/match_events.json'
+    datatuple=str(date)+" "+str(label)
+    sqlcontext=SQLContext(sc)
+    df=sqlcontext.read.option('multiLine',True).json(match_events)
+    try:
+        k=df[datatuple]
+        print("YES")        
+    except:
+        print("NO")
+    '''
+    except:
+        fulldata={}
+    fulldata[datatuple]={}
+    fulldata[datatuple]['date']=date
+    fulldata[datatuple]['duration']=matchdict['duration']
+    fulldata[datatuple]['gameweek']=matchdict['gameweek']
+    fulldata[datatuple]['venue']=matchdict['venue']
+    t1,t2=list(matchdict['teamsData'].keys())
+    n1,n2=label.split(",")[0].split("-")
+    n1=n1.strip()
+    n2=n2.strip()
+    
+    if winner=="0":
+        fulldata[datatuple]['winner']="NULL"
+    else:
+        if winner==t1:
+            fulldata[datatuple]['winner']=n1
+        else:
+            fulldata[datatuple]['winner']=n2
+    fulldata[datatuple]['goals']=[]
+    fulldata[datatuple]['own_goals']=[]
+    fulldata[datatuple]['yellow_cards']=[]
+    fulldata[datatuple]['red_cards']=[]
+    
+    for i in [(t1,n1),(t2,n2)]:
+            j=matchdict["teamsData"][i[0]]
+            print("\n\n",j,"\n\n")
+            if int(j['hasFormation']):
+
+                for k in j['formation']['bench']:
+                    if int(k["ownGoals"])>0:
+                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["ownGoals"]}
+                        fulldata[datatuple]['own_goals'].append(di)
+                    if int(k["goals"])>0:
+                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["goals"]}
+                        fulldata[datatuple]['goals'].append(di)
+                    if int(k['redCards']):
+                        fulldata[datatuple]['red_cards'].append(k["playerId"])
+                    if int(k['yellowCards']):
+                        fulldata[datatuple]['yellow_cards'].append(k["playerId"])
+
+                for k in j['formation']['lineup']:
+                    if int(k["ownGoals"])>0:
+                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["ownGoals"]}
+                        fulldata[datatuple]['own_goals'].append(di)
+                    if int(k["goals"])>0:
+                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["goals"]}
+                        fulldata[datatuple]['goals'].append(di)
+                    if int(k['redCards']):
+                        fulldata[datatuple]['red_cards'].append(k["playerId"])
+                    if int(k['yellowCards']):
+                        fulldata[datatuple]['yellow_cards'].append(k["playerId"])
+
+    json_file=open(match_events,"w")
+    finalobj=json.dumps(fulldata,indent=2)
+    json_file.write(finalobj)
+    '''
+
 
 def printkaro(records):
     print("\n\n",records)  
@@ -125,7 +252,7 @@ lines.foreachRDD(splitrdd)
 
 
 ssc.start()
-ssc.awaitTermination(600)
+ssc.awaitTermination(200)
 ssc.stop()
 
 '''
@@ -136,16 +263,6 @@ lines.reduce(add).pprint()
 ssc.start()
 ssc.awaitTermination()
 '''
-
-
-
-
-
-
-
-
-
-
 '''
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -155,3 +272,5 @@ lines = ssc.socketTextStream("localhost", 9999)
 ssc.start()             # Start the computation
 ssc.awaitTermination()
 '''
+"""
+
--- PROJECT_FPL_ANALYTICS/mainspark.py
+++ PROJECT_FPL_ANALYTICS/mainspark.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from __future__ import print_function
 import findspark
 findspark.init()
 
@@ -14,146 +15,115 @@ from pyspark import SparkContext,SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.sql import Row,SQLContext,SparkSession
 
+def combine(new_list,old_list):
+    if not(old_list):
+        old_list=[]
+    return new_list+old_list
+        
 def eventupdate(i):
     return
 
 def matchupdate(matchdict,date,label):
-    print("\n\nMATCHUPDATE\n\n")
-    match_events='/home/sreyans/Desktop/SEM5/Big_Data_SEM5/PROJECT_FPL_ANALYTICS/match_events.json'
     datatuple=str(date)+" "+str(label)
-    try:
-        spark=SparkSession.builder.appName("FPL_BUILDER").getOrCreate()
-        json_df=spark.read.json(match_events)
-        json_df.printSchema()
-    except:
-        pass
-    '''
-    except:
-        fulldata={}
-    fulldata[datatuple]={}
-    fulldata[datatuple]['date']=date
-    fulldata[datatuple]['duration']=matchdict['duration']
-    fulldata[datatuple]['gameweek']=matchdict['gameweek']
-    fulldata[datatuple]['venue']=matchdict['venue']
+    di={}
+    di['date']=date
+    di['duration']=matchdict['duration']
+    di['gameweek']=matchdict['gameweek']
+    di['venue']=matchdict['venue']
     t1,t2=list(matchdict['teamsData'].keys())
     n1,n2=label.split(",")[0].split("-")
     n1=n1.strip()
     n2=n2.strip()
     winner=matchdict['winner']
     if winner=="0":
-        fulldata[datatuple]['winner']="NULL"
+        di['winner']="NULL"
     else:
         if winner==t1:
-            fulldata[datatuple]['winner']=n1
+            di['winner']=n1
         else:
-            fulldata[datatuple]['winner']=n2
-    fulldata[datatuple]['goals']=[]
-    fulldata[datatuple]['own_goals']=[]
-    fulldata[datatuple]['yellow_cards']=[]
-    fulldata[datatuple]['red_cards']=[]
-    
+            di['winner']=n2
+    di['goals']=[]
+    di['own_goals']=[]
+    di['yellow_cards']=[]
+    di['red_cards']=[]
     for i in [(t1,n1),(t2,n2)]:
-            j=matchdict["teamsData"][i[0]]
-            print("\n\n",j,"\n\n")
-            if int(j['hasFormation']):
+        j=matchdict["teamsData"][i[0]]
+        if int(j['hasFormation']):
+            for k in j['formation']['bench']:
+                #print((k["playerId"]),str(k["playerId"]))
+                name=broadcastplayers.value[str(k["playerId"])]
+                #print(name)
+                if int(k["ownGoals"])>0:
+                    dic={"name":name,"team":i[1],"number_of_own_goals":k["ownGoals"]}
+                    di['own_goals'].append(dic)
+                if int(k["goals"])>0:
+                    dic={"name":name,"team":i[1],"number_of_goals":k["goals"]}  
+                    di["goals"].append(dic)
+                if int(k['redCards']):
+                    di['red_cards'].append(name)
+                if int(k['yellowCards']):
+                    di['yellow_cards'].append(name)
+            for k in j['formation']['lineup']:
+                #print((k["playerId"]),str(k["playerId"]))
+                name=broadcastplayers.value[str(k["playerId"])]
+                if int(k["ownGoals"])>0:
+                    dic={"name":name,"team":i[1],"number_of_own_goals":k["ownGoals"]}
+                    di['own_goals'].append(dic)
+                if int(k["goals"])>0:
+                    dic={"name":name,"team":i[1],"number_of_goals":k["goals"]}  
+                    di["goals"].append(dic)
+                if int(k['redCards']):
+                    di['red_cards'].append(name)
+                if int(k['yellowCards']):
+                    di['yellow_cards'].append(name)
+    return di
 
-                for k in j['formation']['bench']:
-                    if int(k["ownGoals"])>0:
-                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["ownGoals"]}
-                        fulldata[datatuple]['own_goals'].append(di)
-                    if int(k["goals"])>0:
-                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["goals"]}
-                        fulldata[datatuple]['goals'].append(di)
-                    if int(k['redCards']):
-                        fulldata[datatuple]['red_cards'].append(k["playerId"])
-                    if int(k['yellowCards']):
-                        fulldata[datatuple]['yellow_cards'].append(k["playerId"])
 
-                for k in j['formation']['lineup']:
-                    if int(k["ownGoals"])>0:
-                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["ownGoals"]}
-                        fulldata[datatuple]['own_goals'].append(di)
-                    if int(k["goals"])>0:
-                        di={"name_id":k["playerId"],"team":i[1],"number_of_goals":k["goals"]}
-                        fulldata[datatuple]['goals'].append(di)
-                    if int(k['redCards']):
-                        fulldata[datatuple]['red_cards'].append(k["playerId"])
-                    if int(k['yellowCards']):
-                        fulldata[datatuple]['yellow_cards'].append(k["playerId"])
-
-    json_file=open(match_events,"w")
-    finalobj=json.dumps(fulldata,indent=2)
-    json_file.write(finalobj)
-    '''
-def splitandcheck(lines):
-    print("\n\n",lines) 
+def checkmatches(lines): 
     i=json.loads(lines)
     if "wyId" in i:
-        print("\n\nHERE\n\n")
-        matchupdate(i,i["dateutc"].split(" ")[0],i["label"])
+        dateutc=i["dateutc"].split(" ")[0]
+        label=i["label"]
+        yield ("matches",{str(dateutc)+" "+str(label):matchupdate(i,i["dateutc"].split(" ")[0],i["label"])})
     else:
-        eventupdate(i)
+        yield ("events",lines)
 
 
-def splitrdd(records):
-    if (records.isEmpty()):
-        return
-    records.foreach(splitandcheck)
-
-def printkaro(records):
-    print("\n\n",records)  
-
-'''
-#native python code ->to preprocess all the players before any match starts
-
-with open('playerprofile.csv', 'w', newline='') as outcsv:
-    writer = csv.writer(outcsv)
-    writer.writerow(['name','birthArea','birthDate','foot','role','height','passportArea','weight','fouls','goals','own_goals','percent_pass_accuracy','percent_shots_on_target','matches_played'])
-
-    with open('/home/sreyans/Desktop/SEM5/Big_Data_SEM5/PROJECT_FPL_ANALYTICS/play.csv', 'r', newline='') as incsv:
-        reader = csv.reader(incsv)
-        writer.writerows(row + [0,0,0,0,0,0] for row in reader if "name" not in row)
-'''
 conf=SparkConf()
 conf.setAppName('BD_FPL_Project')
-sc=SparkContext.getOrCreate(conf=conf)
+spark=SparkSession.builder.appName("FPL_analytics").getOrCreate()
 
-ssc=StreamingContext(sc,1) #5s for time difference between two batches - design decision
+playercsv=spark.read.option('header',True).csv("/home/sreyans/Desktop/SEM5/Big_Data_SEM5/PROJECT_FPL_ANALYTICS/players.csv")
+playercsv=playercsv.select("Id","name").rdd.collectAsMap()#makes a dictionary of all players
+
+
+sc = spark.sparkContext
+broadcastplayers=sc.broadcast(playercsv)#available to all workers
+#print(broadcastplayers.value["20612"])
+
+ssc=StreamingContext(sc,8) #5s for time difference between two batches - design decision
 ssc.checkpoint('checkpoint_FPL')
-
 lines = ssc.socketTextStream("localhost", 6100)
-#lines.pprint()
-lines.foreachRDD(splitrdd)
 
+matches=lines.flatMap(checkmatches)
+#matches.pprint()
+matches.map(lambda x:(x[0],x[1]))
+matches=matches.groupByKey().mapValues(list)
+matches.pprint(1)
+#print(matches.keys())
+allmatches=matches.updateStateByKey(combine)
+allmatches=allmatches.filter(lambda x:x[1] if x[0]=='matches' else "")
+allmatches.pprint()
+#print(allmatches.value['matches'])
 
+#lines.foreachRDD(splitrdd)
 ssc.start()
-ssc.awaitTermination(100)
+ssc.awaitTermination(20)#giving soome extra time for computations
 ssc.stop()
 
 '''
-sc = SparkContext("local[1]", "NetworkFPL") #2 represents number of threads
-ssc = StreamingContext(sc, 5) #1 represents the batch difference
-lines = ssc.socketTextStream("localhost", 6100)
-lines.reduce(add).pprint()
-ssc.start()
-ssc.awaitTermination()
-'''
-
-
-
-
-
-
-
-
-
-
-'''
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-sc = SparkContext("local[2]", "NetworkWordCount") #2 represents number of threads
-ssc = StreamingContext(sc, 1) #1 represents the batch difference
-lines = ssc.socketTextStream("localhost", 9999)
-ssc.start()             # Start the computation
-ssc.awaitTermination()
+#print(playercsv) dictionary with all players
+#sc=SparkContext.getOrCreate(conf=conf)
+#matchupdate({},"2017-08-11","Arsenal - Leicester City, 4 - 3")
+#all events of a particular match will be streamed together
 '''
